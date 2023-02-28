c8d3ac5c6fb5f2cdc204beb499fd66921b45cdb4
Yaroslav37 <minenkovyaroslav@mail.ru>
2023-03-01 01:26:47 +0300

    added func returning even numbers in a list

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -12,4 +12,20 @@ def calculate(x, y, operation):
             print("division by zero is impossible")
         else:
             return x / y
+        
+
+def get_even_numbers(numbers):
+    even_numbers = []
+
+    for number in numbers:
+        if number % 2 == 0:
+            even_numbers.append(number)
+    
+    return even_numbers
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+result = get_even_numbers(numbers)
+
+print(result)
 
