edaa749982012ab7049d936f5e9a26c636854d63
Yaroslav37 <minenkovyaroslav@mail.ru>
2023-03-01 00:56:54 +0300

    added simple calculate func

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,2 +1,12 @@
 print("Hello, world!")
 
+def calculate(x, y, operation):
+    if operation == 'add':
+        return x + y
+    elif operation == 'sub':
+        return x - y
+    elif operation == 'mult':
+        return x * y
+    elif operation == 'div':
+        return x / y
+
